37dc915c0b20e4454d75bc23d1df7561f87e739a
Askarov Erik <linarizkrp@mail.ru>
2023-04-02 15:32:08 +0300

    П О Б Е Д А

--- app/test_main.py
+++ app/test_main.py
@@ -11,6 +11,9 @@ def test_get_fen_code():
   # response = client.post('/uploadfile', files=_files)
   response = client.get(
       # "/fen", files={"file": ("filename", open('./app/media/test.jpeg', "rb"), "image/jpeg")}
-      "/fen?code=test"
+      "/fen?code=a672f54f-37d4-4c81-87d6-4d95786483a7"
   )
   assert response.status_code == 200
+  assert response.json == {
+    "fen": "a672f54f-37d4-4c81-87d6-4d95786483a7"
+  }
