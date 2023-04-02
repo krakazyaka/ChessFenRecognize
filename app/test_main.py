b1698f90c0ce9b52b0b78c26eb45bb09bfc69ee9
Askarov Erik <linarizkrp@mail.ru>
2023-04-02 15:42:12 +0300

    И Т О Г О В А Я  П О Б Е Д А

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,4 @@ def test_get_fen_code():
       "/fen?code=a672f54f-37d4-4c81-87d6-4d95786483a7"
   )
   assert response.status_code == 200
-  assert response.json() == {
-    "1b1b2k1-K2B1q2-R3B2p-3b1NR1-5p2-3N4-8-5N2"
-  }
+  assert response.json() == '1b1b2k1-K2B1q2-R3B2p-3b1NR1-5p2-3N4-8-5N2'
