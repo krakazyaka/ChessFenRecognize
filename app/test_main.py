10386b4e6d4128680c5df79790e5b1eb2a31786d
Askarov Erik <linarizkrp@mail.ru>
2023-04-02 14:37:37 +0300

    add test file

---
+++ app/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+def test_get_fen_code():
+  _test_upload_file = Path('./media', 'test.jpeg')
+  _files = {'upload_file': _test_upload_file.open('rb')}
+
+  response = client.post('/uploadfile', files=_files)
+
+  assert response.status_code == HTTPStatus.CREATED
+  assert response.status_code == 200
