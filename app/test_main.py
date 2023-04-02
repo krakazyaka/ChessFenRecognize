83fdbd2d9a35fd97a27864c5dfd971dc16a47448
Askarov Erik <linarizkrp@mail.ru>
2023-04-02 15:25:51 +0300

    yes+7

--- app/test_main.py
+++ app/test_main.py
@@ -5,11 +5,12 @@ from .main import app
 client = TestClient(app)
 
 def test_get_fen_code():
-  _test_upload_file = Path('./app/media', 'test.jpeg')
-  _files = {'upload_file': _test_upload_file.open('rb')}
+  # _test_upload_file = Path('./app/media', 'test.jpeg')
+  # _files = {'upload_file': _test_upload_file.open('rb')}
 
   # response = client.post('/uploadfile', files=_files)
-  response = client.post(
-      "/upload_file", files={"file": ("filename", open('./app/media/test.jpeg', "rb"), "image/jpeg")}
+  response = client.get(
+      # "/fen", files={"file": ("filename", open('./app/media/test.jpeg', "rb"), "image/jpeg")}
+      "/fen?code=test"
   )
   assert response.status_code == 200
