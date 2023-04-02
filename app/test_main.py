48f67e187aeaf8b7127204ec2157b11c4dec0953
Askarov Erik <linarizkrp@mail.ru>
2023-04-02 15:21:47 +0300

    yes+6

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,8 @@ def test_get_fen_code():
   _test_upload_file = Path('./app/media', 'test.jpeg')
   _files = {'upload_file': _test_upload_file.open('rb')}
 
-  response = client.post('/uploadfile', files=_files)
-
+  # response = client.post('/uploadfile', files=_files)
+  response = client.post(
+      "/upload_file", files={"file": ("filename", open('./app/media/test.jpeg', "rb"), "image/jpeg")}
+  )
   assert response.status_code == 200
